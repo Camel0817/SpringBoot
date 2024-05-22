28a7cd7a83dec133cef8db233b69edd21361ac87
GyuJin-_-0817 <jkj817@gmail.com>
2024-05-22 21:12:35 +0900

    Spring Bean 등록, 의존성 주입

--- studySpringBoot/src/main/java/com/example/studySpringBoot/product/ProductController.java
+++ studySpringBoot/src/main/java/com/example/studySpringBoot/product/ProductController.java
@@ -1,18 +1,23 @@
 package com.example.studySpringBoot.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/*
+ * "사용자(화면) 요청을 던지면" 그걸 받아서
+ * 요청에 맞는 메소드를 실행 시킵니다.
+ * 그 안에서, 그에 맞는 로직을 수행할 수 있도록 서비스한테 시킬거에요.
+ */
+
 @Controller
 @ResponseBody
 public class ProductController {
-	/*
-	 * "사용자(화면) 요청을 던지면" 그걸 받아서
-	 * 요청에 맞는 메소드를 실행 시킵니다.
-	 * 그 안에서, 그에 맞는 로직을 수행할 수 있도록 서비스한테 시킬거에요.
-	 */
+	
+	@Autowired
+	private ProductService productService;
 	
 	/*
 	 * HTTP method 
@@ -27,7 +32,7 @@ public class ProductController {
 	 */
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String getProduct() {
-		ProductService productService = new ProductService();
+		// ProductService productService = new ProductService();
 		return productService.getProduct();
 	}
 }
--- studySpringBoot/src/main/java/com/example/studySpringBoot/product/ProductService.java
+++ studySpringBoot/src/main/java/com/example/studySpringBoot/product/ProductService.java
@@ -4,8 +4,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+	/*
+	 * 스프링 빈으로 등록해줘! 
+	 * 1. @Component 
+	 * 2. @Configuration + @Bean
+	 * 
+	 * 등록된 빈을 사용해줘!
+	 * 1. @Autowired
+	 */
 	
 	public String getProduct() {
-		return "NoteBook!";
+		return "NoteBook!!";
 	}
 }
