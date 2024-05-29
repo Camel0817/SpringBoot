ac958f8e86bd2e8730629e3ff76e6f2936d5359a
GyuJin-_-0817 <jkj817@gmail.com>
2024-05-29 20:19:15 +0900

    어노테이션 변경(줄임)

--- studySpringBoot/src/main/java/com/example/studySpringBoot/product/ProductController.java
+++ studySpringBoot/src/main/java/com/example/studySpringBoot/product/ProductController.java
@@ -1,13 +1,17 @@
 package com.example.studySpringBoot.product;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
  * "사용자(화면) 요청을 던지면" 그걸 받아서
@@ -15,8 +19,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 그 안에서, 그에 맞는 로직을 수행할 수 있도록 서비스한테 시킬거에요.
  */
 
-@Controller
-@ResponseBody
+//@Controller
+//@ResponseBody
+@RestController
 public class ProductController {
 	
 	@Autowired
@@ -32,14 +37,17 @@ public class ProductController {
 	 */
 	
 	/* 
-	 * 상품 조회
+	 * 개별 상품 조회
 	 */
-	@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+//	@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+	@GetMapping("/products/{id}")
 	public Product getProduct(@PathVariable("id") int id) {
 		// ProductService productService = new ProductService();
 		System.out.println(id);
 		return productService.getProduct(id);
 	}
+	
+
 	
 	/*
 	 * 요청을 받을 때 데이터도 같이 받는 방법
@@ -56,7 +64,8 @@ public class ProductController {
 	/* 
 	 * 상품 등록
 	 */
-	@RequestMapping(value = "/products", method = RequestMethod.POST)
+//	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	@PostMapping("/products")
 	public void saveProduct(@RequestBody Product product) {
 //	public void saveProduct(@RequestParam(value = "name") String productName) {
 		// localhost:8080/products?name=______ => productName
