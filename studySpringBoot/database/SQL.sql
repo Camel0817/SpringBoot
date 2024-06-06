486e63123277aa4c246bc98dcff00338d1c69044
GyuJin-_-0817 <jkj817@gmail.com>
2024-06-06 21:23:49 +0900

    SQL문 추가 Product에 id 추가

---
+++ studySpringBoot/database/SQL.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE shoppingmall;
+
+CREATE TABLE product (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL,
+	price INT NOT NULL,
+	description VARCHAR(500),
+	PRIMARY KEY(id);
+)
+
+INSERT INTO product
+VALUES (NULL,"pen", 2000, "black pink");
--- studySpringBoot/src/main/java/com/example/studySpringBoot/product/Product.java
+++ studySpringBoot/src/main/java/com/example/studySpringBoot/product/Product.java
@@ -2,11 +2,18 @@ package com.example.studySpringBoot.product;
 
 public class Product {
 	
+	private int id;
 	private String name;
 	private int price;
 	private String description;
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
