bd09382c249d641b34c1d490ccba4c6dd8b54dce
GyuJin-_-0817 <jkj817@gmail.com>
2024-06-06 18:58:10 +0900

    JPA와 Spring Data JPA의 차이

--- studySpringBoot/src/main/java/com/example/studySpringBoot/StudySpringBootApplication.java
+++ studySpringBoot/src/main/java/com/example/studySpringBoot/StudySpringBootApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudySpringBootApplication {
-
+//	Spring은 자바로 웹을 만들수 있는 프레임워크
+//	웹 : 정보 공유 -> *DATA*
+//	Spring은 우리가 JPA를 더 편하게 쓸 수 있게 도와주고 싶다.
+//	=> "Spring Data JPA"
 	public static void main(String[] args) {
 		SpringApplication.run(StudySpringBootApplication.class, args);
 	}
